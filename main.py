f6f5c6a139596560947d6fe9497a6ef7172750f0
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-03 09:53:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -183,11 +183,26 @@ def chat(request: ChatRequest):
 🔴 Low Stock Products : {low_stock}
 """
             }
-            # Sell Product Command
-     if "sell" in prompt:
-        return {
-           "response": "🛒 Sale command detected. AI is ready to create the sale."
+
+        # Sell Product Command
+if "sell" in prompt:
+    return {
+        "response": "🛒 Sale command detected. AI is ready to create the sale."
     }
+
+
+
+
+        
+            
+
+    
+        
+    
+            
+     
+        
+    
 
         
 
