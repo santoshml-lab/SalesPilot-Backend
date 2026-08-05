0a2b5f8e408f53243704880d9c0ed24cfd89f3b5
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-05 13:07:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -932,7 +932,7 @@ def send_invoice(invoice_no: str):
     if not customer.data:
         raise HTTPException(status_code=404, detail="Customer Not Found")
 
-    email = customer.data[0]["email"]
+    email = "santoshkrsbg36@gmail.com"
 
     # Read PDF and convert to Base64
     import base64
